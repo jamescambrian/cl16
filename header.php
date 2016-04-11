dfbab5feb03e5d0c6b6d1a77ad3a156ca39b7770
James Shaw <jamesshaw@James-MacBook-Pro.local>
2016-04-11 14:47:53 +0200

    changes to responsivebugs, changes to logo and some fonts

--- header.php
+++ header.php
@@ -4,12 +4,13 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
-<link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,700|Lato:400,300,700" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,700|Lato:400,300,700|Open+Sans:800" rel="stylesheet" type="text/css">
 <link rel="icon" type="image/png" href="<?php echo get_bloginfo('template_directory'); ?>/images/favicon.png">
 <?php wp_head(); ?>
 </head>
 <body>
 <nav id="menu" role="navigation" class="pushy pushy-left">
+<img class="" style="max-width:200px; margin-left:2rem;" src="<?php echo get_bloginfo('template_directory'); ?>/images/logo.svg" data-pin-nopin="true">
 <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
 <ul class="social-sidebar">
     <li><a href="https://soundcloud.com/cambrian-line" target="_blank"><img src="<?php echo get_bloginfo('template_directory'); ?>/images/icons/icon-soundcloud.svg"/></a></li>
@@ -20,10 +21,8 @@
 <div class="site-overlay"></div>
 <!-- Site Wrap -->
 <div id="container">
-<header id="header" class="header"><div class="menu-btn">&#9776; Menu</div>
-  <div class="container">
-    <a href="<?php echo site_url(); ?>"><img class="logo" src="<?php echo get_bloginfo('template_directory'); ?>/images/logo.svg" data-pin-nopin="true"></a>
-  </div>
+<header id="header" class="header"><div class="menu-btn">&#9776;</div>
+	<a class="" href="<?php echo site_url(); ?>"><img class="logo" src="<?php echo get_bloginfo('template_directory'); ?>/images/logo.png" data-pin-nopin="true"></a>
 </header>
 <!-- Top Nav Ends -->
 <div id="container">
