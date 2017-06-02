0de4e514891c1a3e37bfaadd4149ea30b1e47e7c
James Shaw <beard45@Oh-Gosh-iMac.local>
2017-06-02 09:26:38 +0200

    beginning work on deleting sliding menu section for web

--- header.php
+++ header.php
@@ -20,6 +20,7 @@
 </script>
 </head>
 <body id="top">
+<!--
 	<div class="overlay overlay-contentpush">
 			<button type="button" class="overlay-close">Close</button>
 			<nav>
@@ -28,11 +29,14 @@
 
 			</nav>
 		</div>
+-->
 <div class="wrap">
 <!-- Site Wrap -->
 <div id="container">
-<header id="header" class="header"><div id="trigger-overlay" class="menu-btn"></div>
-	<a class="" href="<?php echo site_url(); ?>"><img class="logo" src="<?php echo get_bloginfo('template_directory'); ?>/images/logo.png" data-pin-nopin="true"></a>
+<header id="header" class="header">
+<!--    <div id="trigger-overlay" class="menu-btn"></div>-->
+    <div class="menu"><?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?></div>
+<!--	<a class="" href="<?php echo site_url(); ?>"><img class="logo" src="<?php echo get_bloginfo('template_directory'); ?>/images/logo.png" data-pin-nopin="true"></a>-->
 </header>
 <!-- Top Nav Ends -->
 <div id="container"></p>
