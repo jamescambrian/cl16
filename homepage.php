5a19ff2a35fd80cbe6f4fb3afa0d1f3c20484142
James Shaw <beard45@Oh-Gosh-iMac.local>
2017-06-11 15:36:25 +0200

    added mobile friendly in page nav elements for scrolling, fixed some mobile errors

--- homepage.php
+++ homepage.php
@@ -14,7 +14,7 @@ while($catquery->have_posts()) : $catquery->the_post();
 <?php endwhile; ?>
 <?php wp_reset_query(); ?>
 
-
+<a class="mobile-nav" href="<?php echo site_url(); ?>/category/releases/">See All Releases</a>
 
 <?php
 $catquery = new WP_Query( 'category_name=featured&posts_per_page=1' );
@@ -40,24 +40,32 @@ while($catquery->have_posts()) : $catquery->the_post();
 <?php endwhile; ?>
 <?php wp_reset_query(); ?>
 <!-- Featured Post ends -->
+<a class="mobile-nav" href="<?php echo site_url(); ?>/category/news/">See All News</a>
 <!-- Radio Section Begins -->
- <div class="color-wrap color-wrap--deep-purple">
-  <div class="container">
-    <div class="row wrap-title"><img class="logo-small" src="<?php echo get_bloginfo('template_directory'); ?>/images/logo.png"><h2>Latest Radio</h2><br><span><a class="see-more" href="<?php echo site_url(); ?>/category/radio/">See all Radio shows</a></span></div>
-  <div class="row cover-list">
-    <ul class="podcast-covers">
-      <?php
-      $catquery = new WP_Query( 'category_name=radio&posts_per_page=3' );
-      while($catquery->have_posts()) : $catquery->the_post();
-      ?>
-      <li class="one-third column fade-in-element">
-        <figure>
-            <?php the_post_thumbnail( 'full', array( 'class' => 'u-full-width' ) ); ?>
-            <figcaption><!--  <span class="button-play"></span><br>  -->
-              <span class="showtitle"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></span><span class="showtitle__dj"><?php $key="Artist"; echo get_post_meta($post->ID, $key, true); ?></span>
-            </figcaption>
-        </figure>
-      </li>
+ 
+
+<div class="color-wrap color-wrap--deep-purple">
+    <div class="container">
+        <div class="row wrap-title">
+            <img class="logo-small" src="<?php echo get_bloginfo('template_directory'); ?>/images/logo.png">
+            <h2>Latest Radio</h2>
+            <br>
+            <span>
+                <a class="see-more" href="<?php echo site_url(); ?>/category/radio/">See all Radio shows</a></span>
+        </div>
+        <div class="row cover-list">
+            <ul class="podcast-covers">
+                <?php $catquery = new WP_Query( 'category_name=radio&posts_per_page=3' ); while($catquery->have_posts()) : $catquery->the_post(); ?>
+                <li class="one-third column fade-in-element">
+                    <figure>
+                        <?php the_post_thumbnail( 'full', array( 'class' => 'u-full-width' ) ); ?>
+                        <figcaption>
+                            <span class="showtitle">
+                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></span><span class="showtitle__dj"><?php $key="Artist"; echo get_post_meta($post->ID, $key, true); ?>
+                            </span>
+                        </figcaption>
+                    </figure>
+                </li>
      <?php endwhile; ?>
      <?php wp_reset_query(); ?>
     </ul>
@@ -65,6 +73,7 @@ while($catquery->have_posts()) : $catquery->the_post();
 </div>
 </div>
 <br class="u-cf"/>
+<a class="mobile-nav" href="<?php echo site_url(); ?>/category/radio/">See All Radio</a>
 <?php
 $catquery = new WP_Query( 'category_name=mixtapes&posts_per_page=1' );
 while($catquery->have_posts()) : $catquery->the_post();
@@ -73,7 +82,7 @@ while($catquery->have_posts()) : $catquery->the_post();
 <div class="post__large fade-in-element">
   <div class="post__large--feat">
     <div class="post__large--excerpt">
-    <span class="home__cat__title">mixtape</span>  
+    <span class="home__cat__title">Latest mixtape</span>  
       <h2><a href="<?php the_permalink(); ?>" class="titles"><?php the_title(); ?></a></h2>
       <?php the_excerpt(); ?>
       <a class="button button-read-more button-read-more-feat hvr-sweep-to-right " href="<?php the_permalink(); ?> ">read more</a>
@@ -88,5 +97,28 @@ while($catquery->have_posts()) : $catquery->the_post();
 </div>
 <?php endwhile; ?>
 <?php wp_reset_query(); ?>
-<!-- Featured Post ends -->
 
+<a class="mobile-nav" href="<?php echo site_url(); ?>/category/mixtapes/">See All Mixtapes</a>
+<!-- Featured Post ends -->
+<!-- Radio Section Begins -->
+ <div class="color-wrap color-wrap--yellow">
+  <div class="container">
+  <div class="row cover-list">
+    <ul class="podcast-covers">
+      <?php
+      $catquery = new WP_Query( 'category_name=blog&posts_per_page=6' );
+      while($catquery->have_posts()) : $catquery->the_post();
+      ?>
+      <li class="one-third column article fade-in-element">
+           <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <?php the_excerpt('2'); ?>
+      <a class="link" href="<?php the_permalink(); ?> ">read more</a>
+      </li>
+     <?php endwhile; ?>
+     <?php wp_reset_query(); ?>
+    </ul>
+  </div>
+</div>
+</div>
+  <br class="u-cf"/>
+<a class="mobile-nav" href="<?php echo site_url(); ?>/category/blog/">See All Blog Posts</a>
